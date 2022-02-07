19344609ca25474b70e0142795a40864c2fed0d9
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-02-07 23:18:21 +0900

    remake repository

---
+++ memo_app.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'date'
+require 'csv'
+require 'sinatra/reloader' if development?
+enable :method_override
+
+set :environment, :production
+
+helpers do
+  def escape(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+get '/' do
+  redirect '/memos'
+end
+
+# メモ一覧表示
+get '/memos' do
+  @memos = Dir.glob('./memos/*')
+  erb :memos
+end
+
+# メモ追加
+post '/memos' do
+  File.open("./memos/#{params[:title]}.csv", 'w', encoding: 'Shift_JIS:UTF-8') do |file|
+    file.write(params[:content])
+  end
+  erb :new
+  redirect '/memos'
+end
+
+# 新規メモ入力
+get '/memos/new' do
+  erb :new
+end
+
+# メモ表示
+get '/memos/:title' do
+  @memo = File.open("./memos/#{params[:title]}", encoding: 'Shift_JIS:UTF-8')
+  erb :show
+end
+
+# メモ編集画面を開く
+get '/memos/:title/edit' do
+  @memo = File.open("./memos/#{params[:title]}", encoding: 'Shift_JIS:UTF-8')
+  erb :edit
+end
+
+# メモ編集
+patch '/memos' do
+  File.open("./memos/#{params[:title]}", 'w', encoding: 'Shift_JIS:UTF-8') do |file|
+    file.write(params[:content])
+  end
+end
+
+# メモ削除
+delete '/memos/:title/delete' do
+  File.delete("./memos/#{params[:title]}")
+  redirect '/memos'
+end
